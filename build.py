e2384a969b4da4ca300234ff5c30ed7a6f0bc2cd
Bálint Gonda <balinteus@gmail.com>
2021-07-18 13:49:30 +0200

    Create build util

---
+++ build.py
@@ -0,0 +1,14 @@
+import os
+import PyInstaller.__main__
+
+image_folder = "img" + os.pathsep + "img"
+
+PyInstaller.__main__.run([
+    "main.py",
+    "--name", "QR-Signer",
+    "--onefile",
+    "--windowed",
+    "--add-binary", image_folder, 
+    "--icon", "img/icon.ico"
+])
+
