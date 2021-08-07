8fac2514ba82cceae9a5950f3475c264f396c92f
Bálint Gonda <balinteus@gmail.com>
2021-08-07 20:38:36 +0200

    Document code & Make the codebase PEP-8 compliant

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
 import io
-import html.parser
-import base64
 import sys
-from PIL import Image, ImageDraw, ImageFilter
+import base64
+import html.parser
+
+from PIL import Image
 import PySimpleGUI as pgui
 
 pgui.theme("DarkBrown4")
@@ -20,6 +22,7 @@ empty_image = image_dir + "empty.png"
 multiple_image = image_dir + "multiple.png"
 arrow_image = image_dir + "arrow.png"
 
+# Global Variables
 qr_paths = []
 ps_paths = []
 html_splits = []
@@ -37,15 +40,16 @@ output_size = 85
 # Constants
 A4_SIZE = (2480, 3508)
 
+# PySimpleGUI Layouts
 # fmt: off
-menubarLayout = [
+menubar_layout = [
     ["&File", ["&Import...", ["&QR Code(s)", "&Signature Image"], "Specify &export location", "---", "!&Generate", "!&Print", "&Clear inputs", "---", "E&xit"]],
     ["&Toolbox", ["&HTML sheet splitter", "&Print sheet generator"]],
     ["Se&ttings"],
     ["&About", ["!Made by: Balinteus", "---",  "My website", "The project's github page"]]
 ]
 
-generatorLayout = [
+generator_layout = [
     [pgui.Column([ [pgui.Frame("Imported QR Code", [[pgui.Image(empty_image, key="-qr_img-", size=(300, 300))]])],
                  [pgui.Button("Import QR Code(s)", size=[38, 1], key="-import_qr-")],
                  [pgui.Button("Import Signature Image", size=[38, 1], key="-import_sign-")] ]),
@@ -55,7 +59,7 @@ generatorLayout = [
                  [pgui.Button("Generate", size=[38, 1], disabled=True, key="-generate-")] ])]
 ]
 
-htmlsplitterLayout = [
+htmlsplitter_layout = [
     [pgui.Column([[pgui.Frame("HTML sheet splitter", [[pgui.Button("Import HTML sheet", size=[38, 1], key="-hs_import-")],
                 [pgui.HorizontalSeparator(pad=(10, 10))],
                 [pgui.Button("Save QR Code(s)", size=[38, 1], key="-hs_save-", disabled=True)],
@@ -64,7 +68,7 @@ htmlsplitterLayout = [
                 [pgui.Button("Export QR Code(s) into Print Sheet Generator", size=[38, 1], key="-hs_exp_ps-", disabled=True)] ], pad=(225, 100)) ]]) ]
 ]
 
-printsheetLayout = [
+printsheet_layout = [
     [pgui.Column([ [pgui.Frame("Print sheet generator", [
                       [pgui.Button("Import QR Code(s)", size=[38, 1], key="-ps_imp-")],
                       [pgui.HorizontalSeparator(pad=(10, 10))],
@@ -77,33 +81,36 @@ printsheetLayout = [
     pgui.Column([ [pgui.Frame("Output", [[pgui.Image(empty_image, key="-ps_img-", size=(248, 350))]])]])]
 ]
 
-settingsLayout = [
+settings_layout = [
     [pgui.Text("Settings")],
     [pgui.Text("Imported QR Code size: "), pgui.Input()],
     [pgui.Text("Signature image size (on generated img): "), pgui.Input()],
 ]
 
-tabLayout = [
+tab_layout = [
     [
-        pgui.Tab("Signature generator", generatorLayout),
-        pgui.Tab("HTML sheet splitter", htmlsplitterLayout),
-        pgui.Tab("Print sheet generator", printsheetLayout),
-        pgui.Tab("Settings", settingsLayout),
+        pgui.Tab("Signature generator", generator_layout),
+        pgui.Tab("HTML sheet splitter", htmlsplitter_layout),
+        pgui.Tab("Print sheet generator", printsheet_layout),
+        pgui.Tab("Settings", settings_layout),
     ]
 ]
 
-mainLayout = [ 
-    [pgui.Menu(menubarLayout)],
+main_layout = [ 
+    [pgui.Menu(menubar_layout)],
     [pgui.Text("Balinteus' Munzee Toolbox", size=[46, 1], font="Helvitica 20 bold", justification="center", relief=pgui.RELIEF_RIDGE)],
-    [pgui.TabGroup(tabLayout, tab_location="topleft")]
+    [pgui.TabGroup(tab_layout, tab_location="topleft")]
 ]
 # fmt: on
 
-mainWindow = pgui.Window("Munzee Toolbox", mainLayout, icon=icon_image)
+main_window = pgui.Window("Munzee Toolbox", main_layout, icon=icon_image)
 
 
-def readyCheck():
+def ready_check():
+    """Checks if all the required inputs are fulfilled. Enables/disables buttons
+    if these requirements are met."""
     print("Ready check!")
+    # SG Ready check
     if (
         (len(qr_paths) > 0)
         and (sign_path != "")
@@ -111,28 +118,45 @@ def readyCheck():
         and (export_location != "")
         and (export_location != None)
     ):
-        mainWindow.Element("-generate-").Update(disabled=False)
-        print("READY!")
+        main_window.Element("-generate-").Update(disabled=False)
+        print("SG READY!")
     else:
-        mainWindow.Element("-generate-").Update(disabled=True)
+        main_window.Element("-generate-").Update(disabled=True)
+    # PSG Ready check
     if len(ps_paths) > 0:
-        mainWindow.Element("-ps_gen-").Update(disabled=False)
-        mainWindow.Element("-ps_save-").Update(disabled=False)
+        main_window.Element("-ps_gen-").Update(disabled=False)
+        main_window.Element("-ps_save-").Update(disabled=False)
+        print("PSG READY!")
     else:
-        mainWindow.Element("-ps_gen-").Update(disabled=True)
-        mainWindow.Element("-ps_save-").Update(disabled=True)
+        main_window.Element("-ps_gen-").Update(disabled=True)
+        main_window.Element("-ps_save-").Update(disabled=True)
     # HS Ready check
     if len(html_splits) > 0:
-        mainWindow.Element("-hs_save-").Update(disabled=False)
-        mainWindow.Element("-hs_exp_sg-").Update(disabled=False)
-        mainWindow.Element("-hs_exp_ps-").Update(disabled=False)
+        main_window.Element("-hs_save-").Update(disabled=False)
+        main_window.Element("-hs_exp_sg-").Update(disabled=False)
+        main_window.Element("-hs_exp_ps-").Update(disabled=False)
+        print("HS READY!")
     else:
-        mainWindow.Element("-hs_save-").Update(disabled=True)
-        mainWindow.Element("-hs_exp_sg-").Update(disabled=True)
-        mainWindow.Element("-hs_exp_ps-").Update(disabled=True)
+        main_window.Element("-hs_save-").Update(disabled=True)
+        main_window.Element("-hs_exp_sg-").Update(disabled=True)
+        main_window.Element("-hs_exp_ps-").Update(disabled=True)
 
 
-def generateSignature(qr_path, sign_path, isThumbnail=False, isBinary=False):
+def generate_signature(qr_path, sign_path, isThumbnail=False, isBinary=False):
+    """Generates a signatured image by combining two images, a background image
+    (in our case this will a QR code) and a signature image.
+
+    Args:
+        qr_path: Path to the background image (in our case a QR code).
+        sign_path: Path to the signature image.
+        isThumbnail (bool, optional): Skips the resize process. Usually used on
+            thumbnail generations. Defaults to False.
+        isBinary (bool, optional): Set to True if the two input images are
+            PIL.Image objects. Defaults to False.
+
+    Returns:
+        PIL.Image object: The merged image.
+    """
     # Load the base images
     if not isBinary:
         qr_img = Image.open(qr_path)
@@ -159,7 +183,17 @@ def generateSignature(qr_path, sign_path, isThumbnail=False, isBinary=False):
     return generated_img
 
 
-def generateThumbnail(base_image, size=(300, 300)):
+def generate_thumbnail(base_image, size=(300, 300)):
+    """Creates a thumbnailed version of a given image and saves it to memory.
+
+    Args:
+        base_image (string): This image will be thumbnailed.
+        size (tuple, optional): The size of the generated thumbnail.
+            Defaults to (300, 300).
+
+    Returns:
+        PIL.Image object: The thumbnailed version of the image.
+    """
     # Create a copy of the base image, this way we don't overwrite it
     work_image = base_image.copy()
 
@@ -173,63 +207,95 @@ def generateThumbnail(base_image, size=(300, 300)):
     return generated_thumbnail
 
 
-def updateThumbnails():
+def update_thumbnails():
+    """Controls all the GUI Image elements' state."""
     print("Updating thumbnails...")
     # Update SG thumbnails
     if len(qr_paths) > 1:
-        mainWindow.Element("-qr_img-").Update(filename=multiple_image, size=(300, 300))
+        main_window.Element("-qr_img-").Update(filename=multiple_image, size=(300, 300))
         if (sign_path != "") and (sign_path != None):
-            mainWindow.Element("-rendered_img-").Update(
+            main_window.Element("-rendered_img-").Update(
                 filename=multiple_image, size=(300, 300)
             )
     elif len(qr_paths) == 1:
         # Update QR thumbnail
-        qr_thumbnail = generateThumbnail(Image.open(qr_paths[0]))
-        mainWindow.Element("-qr_img-").Update(
+        qr_thumbnail = generate_thumbnail(Image.open(qr_paths[0]))
+        main_window.Element("-qr_img-").Update(
             filename=None, data=qr_thumbnail.getvalue(), size=(300, 300)
         )
         # Update Rendered thumbnail
         if (sign_path != "") and (sign_path != None):
-            rendered_image = generateSignature(qr_paths[0], sign_path, True)
-            rendered_thumbnail = generateThumbnail(rendered_image)
-            mainWindow.Element("-rendered_img-").Update(
+            rendered_image = generate_signature(qr_paths[0], sign_path, True)
+            rendered_thumbnail = generate_thumbnail(rendered_image)
+            main_window.Element("-rendered_img-").Update(
                 filename=None, data=rendered_thumbnail.getvalue(), size=(300, 300)
             )
         else:
-            mainWindow.Element("-rendered_img-").Update(
+            main_window.Element("-rendered_img-").Update(
                 filename=None, data=qr_thumbnail.getvalue(), size=(300, 300)
             )
     else:
-        mainWindow.Element("-qr_img-").Update(filename=empty_image, size=(300, 300))
-        mainWindow.Element("-rendered_img-").Update(
+        main_window.Element("-qr_img-").Update(filename=empty_image, size=(300, 300))
+        main_window.Element("-rendered_img-").Update(
             filename=empty_image, size=(300, 300)
         )
     # Update PS thumbnails
     if printsheet == None:
-        mainWindow.Element("-ps_img-").Update(filename=empty_image, size=(248, 350))
+        main_window.Element("-ps_img-").Update(filename=empty_image, size=(248, 350))
 
 
-def parsePaths(raw_paths: str):
+def parse_paths(raw_paths: str):
+    """Parses file/folder paths from a given string.
+
+    Example for this string:
+        "C:/Users/bali/Desktop/gen.png;C:/Users/bali/Desktop/index.png"
+
+    Args:
+        raw_paths (str): The provided paths in raw format.
+
+    Returns:
+        List: A list filled with the parsed paths.
+    """
     parsed_paths = []
     if (raw_paths != None) and (raw_paths != ""):
         parsed_paths = raw_paths.split(";")
     return parsed_paths
 
 
-def addMargin(base_image_path: str, margin_size: int):
+def add_margin(base_image_path: str, margin_size: int):
+    """Adds a margin to a given image. This process is done by creating a new
+    "background" image thats bigger than the original one and putting the
+    original image into the center of this newly created white background.
+
+    Args:
+        base_image_path (str): The path to the provided image.
+        margin_size (int): This number determines the thickness of the margin.
+
+    Returns:
+        PIL.Image object: The marginized image.
+    """
     base_image = Image.open(base_image_path)
     background = Image.new(
         "RGBA",
         (base_image.width + margin_size * 2, base_image.height + margin_size * 2),
         (255, 255, 255),
     )
-    return generateSignature(background, base_image, True, True)
+    return generate_signature(background, base_image, True, True)
 
 
-def generatePrintsheet(qr_codes: dict):
+def generate_printsheet(qr_codes: list):
+    """Generates a print sheet from the given QR codes.
+
+    Args:
+        qr_codes (list): A list which contains marginized PIL.Image objects. 
+
+    Returns:
+        PIL.Image object: The generated print sheet.
+    """
     # Get generator settings
-    margin_size = mainWindow.Element("-ps_margin-").Get()
-    paper_size = mainWindow.Element("-ps_papertype-").Get()
+    margin_size = main_window.Element("-ps_margin-").Get()
+    paper_size = main_window.Element("-ps_papertype-").Get()
+    # TODO: Rewrite this section to use dicts
     if paper_size == "A4":
         paper_size = A4_SIZE
     else:
@@ -238,7 +304,7 @@ def generatePrintsheet(qr_codes: dict):
     # Marginize images
     marginized_imgs = []
     for i in range(len(qr_codes)):
-        marginized_imgs.append(addMargin(qr_codes[i], margin_size))
+        marginized_imgs.append(add_margin(qr_codes[i], margin_size))
 
     # Create background
     paper = Image.new("RGBA", paper_size, (255, 255, 255))
@@ -263,11 +329,25 @@ def generatePrintsheet(qr_codes: dict):
 
 
 class HTMLReader(html.parser.HTMLParser):
-    def __init__(self, collector):
+    """A custom HTML parser, that collects all the Base64 image data from an
+    HTML input.
+
+    Args:
+        collector (list): All the Base64 encoded images will be placed here.
+    """
+
+    def __init__(self, collector: list):
+        """Inits HTMLReader."""
         html.parser.HTMLParser.__init__(self)
         self.collector = collector
 
     def handle_starttag(self, tag, attrs):
+        """Detects HTML starttags.
+
+        Args:
+            tag (string): The name of the HTML tag.
+            attrs (string): All the attributes of the detected tag.
+        """
         # Only collects the img tags & removes the data prefixes
         if tag == "img":
             self.collector.append(attrs[1][1].split("data:image/png;base64,")[1])
@@ -291,12 +371,22 @@ class HTMLReader(html.parser.HTMLParser):
         pass
 
 
-def htmlSplitter(html_path: str):
+def split_htmlsheet(html_path: str):
+    """Collects all the Base64 encoded images from the provided HTML file. And
+    converts them into PIL.Image objects.
+
+    Args:
+        html_path (str): Path to the provided HTML file.
+
+    Returns:
+        List: A list filled with PIL.Image objects.
+    """
     encoded_imgs = []
     html_file = open(html_path, "r").read()
     parser = HTMLReader(encoded_imgs)
     parser.feed(html_file)
 
+    # Decode images from Base64 strings into PIL Image objects
     decoded_imgs = []
     for i in range(len(encoded_imgs)):
         temp_img = Image.open(io.BytesIO(base64.b64decode(encoded_imgs[i])))
@@ -306,7 +396,7 @@ def htmlSplitter(html_path: str):
 
 
 while True:
-    event, values = mainWindow.read()
+    event, values = main_window.read()
     if event == pgui.WIN_CLOSED or event == "Exit":
         break
     elif event == "Clear inputs":
@@ -329,8 +419,7 @@ while True:
             icon=icon_image,
         )
         print(qr_files)
-        # Example files string: "C:/Users/balin/Desktop/gen.png;C:/Users/balin/Desktop/index.png"
-        qr_paths = parsePaths(qr_files)
+        qr_paths = parse_paths(qr_files)
         print(qr_paths)
     elif event == "-import_sign-" or event == "Signature Image":
         sign_path = pgui.popup_get_file("Choose your signature image!", icon=icon_image)
@@ -343,7 +432,7 @@ while True:
     elif event == "-generate-" or event == "Generate":
         generated_images = []
         for i in range(len(qr_paths)):
-            generated_images.append(generateSignature(qr_paths[i], sign_path))
+            generated_images.append(generate_signature(qr_paths[i], sign_path))
         for i in range(len(generated_images)):
             generated_images[i].save(
                 f"{export_location}{'' if export_location.endswith('/') else '/'}gen_{total_session_gens}_{i}.png"
@@ -359,7 +448,7 @@ while True:
             icon=icon_image,
             file_types=(("HTML files", "*.html"),),
         )
-        html_splits = htmlSplitter(html_path)
+        html_splits = split_htmlsheet(html_path)
     elif event == "-hs_save-":
         export_folder = pgui.popup_get_folder(
             "Choose your output folder!", icon=icon_image
@@ -383,13 +472,13 @@ while True:
             icon=icon_image,
         )
         print(ps_files)
-        ps_paths = parsePaths(ps_files)
+        ps_paths = parse_paths(ps_files)
         print(ps_paths)
     elif event == "-ps_gen-":
-        printsheet = generatePrintsheet(ps_paths)
-        mainWindow.Element("-ps_img-").Update(
+        printsheet = generate_printsheet(ps_paths)
+        main_window.Element("-ps_img-").Update(
             filename=None,
-            data=generateThumbnail(printsheet, (248, 350)).getvalue(),
+            data=generate_thumbnail(printsheet, (248, 350)).getvalue(),
             size=(248, 350),
         )
     elif event == "-ps_save-":
@@ -400,8 +489,8 @@ while True:
             f"{ps_export_location}{'' if ps_export_location.endswith('/') else '/'}printsheet.png"
         )
 
-    readyCheck()
-    updateThumbnails()
+    ready_check()
+    update_thumbnails()
     print(event)
 
-mainWindow.close()
+main_window.close()
